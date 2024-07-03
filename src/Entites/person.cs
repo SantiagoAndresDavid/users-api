12dd3a32dcc0c543be3a1608bf89d67777662e3f
SantiagoAndresDavid <sadavid@unicesar.edu.co>
2024-07-03 12:30:06 -0500

    add entity user

---
+++ src/Entites/person.cs
@@ -0,0 +1,13 @@
+namespace Entites;
+
+public class person
+{
+    public required int IdentificationCard { get; set; }
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }
+    public string Age { get; set; }
+
+    
+}
